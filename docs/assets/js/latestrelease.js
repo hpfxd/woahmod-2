43cedc16cdbfbb7cd5df1ca738751f50aff0d162
hpfxd <natepanda5@gmail.com>
2018-05-08 03:22:46 -0400

    pleaseworkpleaseworkpleaseworkpleasework

--- docs/assets/js/latestrelease.js
+++ docs/assets/js/latestrelease.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
     $.getJSON("https://api.github.com/repos/hpfxd/woahmod-2/releases/latest", function(data) {
-        var count = new countUp("latest", 0.0, 0.7, 1);
+        var ver = data.tag_name;
+        var count = new countUp("latest", 0.0, ver, 1);
         if (!count.error) {
             count.start();
         } else {
             console.error(count.error);
         }
+
+        $(".page-header").append("<a class='btn'" +
+            " href='https://github.com/hpfxd/woahmod-2/releases/download/"+ver+"/woahmod-2-"+ver+".jar'" +
+            " target='_blank'>" +
+            "Download latest release" +
+            "</a>");
     });
 });
