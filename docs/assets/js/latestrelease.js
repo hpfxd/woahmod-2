4426a6147a8a3714d3d5dc4aaea5f046cdf26461
hpfxd <natepanda5@gmail.com>
2018-05-08 02:59:03 -0400

    update website

--- docs/assets/js/latestrelease.js
+++ docs/assets/js/latestrelease.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
     $.getJSON("https://api.github.com/repos/hpfxd/woahmod-2/releases/latest", function(data) {
-        $("#latest").text(data.tag_name);
+        var count = new CountUp("latest", 0.0, 0.7, 1);
+        if (!count.error) {
+            count.start();
+        } else {
+            console.error(count.error);
+        }
     });
 });
