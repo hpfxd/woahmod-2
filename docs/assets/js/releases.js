372ff72dbacb5724e13e780e584ca9a76e53b1bb
hpfxd <natepanda5@gmail.com>
2018-05-08 08:25:59 -0400

    update site :D

--- docs/assets/js/releases.js
+++ docs/assets/js/releases.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
     $.getJSON("https://api.github.com/repos/hpfxd/woahmod-2/releases", function(data) {
-        data.reverse();
         $.each(data, function (i, v) {
             var ver = v.tag_name;
             $("#releases-section").append(
                 "<div>" +
                 "<b>" + v.name + "</b>" + (v.prerelease ? "<i>PRERELEASE</i>" : "") +
-                "<a class='btn' target='_blank' href='https://github.com/hpfxd/woahmod-2/releases/download/" + ver + "/woahmod-2-" + ver + ".jar'>Download v" + ver + "</a>" +
+                " <a style='text-decoration:none;' target='_blank' href='https://github.com/hpfxd/woahmod-2/releases/download/" + ver + "/woahmod-2-" + ver + ".jar'>Download v" + ver + "</a>" +
                 "</div>"
             );
         });
