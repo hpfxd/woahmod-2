685026c0455436b1e5024a4a50ed115140328fe6
hpfxd <natepanda5@gmail.com>
2018-05-09 02:58:41 -0400

    update site

--- docs/assets/js/latestrelease.js
+++ docs/assets/js/latestrelease.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
     $.getJSON("https://api.github.com/repos/hpfxd/woahmod-2/releases/latest", function(data) {
         var ver = data.tag_name;
-        var count = new countUp("latest", 0.0, ver, 2, 3);
+        var count = new countUp("latest", 0.0, ver, 1, 3, {
+            prefix: "v"
+        });
         if (!count.error) {
             count.start();
         } else {
--- docs/assets/js/releases.js
+++ docs/assets/js/releases.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
                 "<div>" +
                 "<p><b>" + v.name + "</b> " + (v.prerelease ? "<i>PRE-RELEASE</i>" : "<i>RELEASE</i>") + "</p> " +
                 "<p><a style='text-decoration:none;' target='_blank' href='https://github.com/hpfxd/woahmod-2/releases/download/" + ver + "/woahmod-2-" + ver + ".jar'>Download .jar</a><br/>" +
-                "    Download source code " +
-                "<a style='text-decoration:none;' target='_blank' href='https://github.com/hpfxd/woahmod-2/archive/"+ver+".tar.gz'>.tar.gz</a> &verbar; " +
-                "<a style='text-decoration:none;' target='_blank' href='https://github.com/hpfxd/woahmod-2/archive/"+ver+".zip'>.zip</a>" +
+                "&#8203;    Download source code &verbar; " +
+                "<a style='text-decoration:none;' target='_blank' href='https://github.com/hpfxd/woahmod-2/archive/"+ver+".tar.gz'>.tar.gz</a> " +
+                "<a style='text-decoration:none;' target='_blank' href='https://github.com/hpfxd/woahmod-2/archive/"+ver+".zip'>"+ver+".zip</a>" +
                 "</p>" +
                 "</div>"
             );
